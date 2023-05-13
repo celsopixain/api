bd79049d9607649e1d5dacd58e27e65041734fe6
Celso Ricardo <celsopixain@gmail.com>
2023-05-12 23:00:36 -0500

    Mapeando a classe JPA

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -12,6 +12,7 @@ public class MedicoController {
 
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
+
         System.out.println(dados);
     }
 }
---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,22 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+}
